e4a5bd3c6fa236e9dd0c7c5a309fd516acf273bb
KirtikumarKavande <kirtikumar0005@gmail.com>
2022-12-23 11:15:55 +0530

    task3

---
+++ script.js
@@ -0,0 +1,7 @@
+mainheader=document.getElementById('main-header') 
+mainheader.style.border='2px solid black' 
+ 
+titlechange=document.querySelector('.title') 
+console.log(titlechange); 
+titlechange.style.color='green' 
+titlechange.style.fontWeight='bold'
