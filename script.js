146f6e9b774d0752b128ed4fe7d9131f9daafe69
KirtikumarKavande <kirtikumar0005@gmail.com>
2022-12-23 16:30:08 +0530

    classnametask4

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-mainheader=document.getElementById('main-header') 
-mainheader.style.border='2px solid black' 
+// mainheader=document.getElementById('main-header') 
+// mainheader.style.border='2px solid black' 
  
-titlechange=document.querySelector('.title') 
-console.log(titlechange); 
-titlechange.style.color='green' 
-titlechange.style.fontWeight='bold'
+// titlechange=document.querySelector('.title') 
+// console.log(titlechange); 
+// titlechange.style.color='green' 
+// titlechange.style.fontWeight='bold'
+
+
+liList=document.getElementsByClassName('list-group-item')
+liList[2].style.backgroundColor='green'
+
+for(i=0;i<liList.length;i++)
+{
+    liList[i].style.fontWeight='bold'
+}
+console.log(liList);
