660127a44e72ecbfd22d1021691a3e438c772f07
KirtikumarKavande <kirtikumar0005@gmail.com>
2022-12-24 11:55:03 +0530

    added description

--- DOM2/script1.js
+++ DOM2/script1.js
@@ -5,18 +5,23 @@ itemsList=document.getElementById('items')
 //delete
 itemsList.addEventListener('click',removeItem)
 
-console.log(itemsList);
+
+
+
+
+
 
 function handleSubmit(e)
 {
     usertext=document.getElementById('item').value
     e.preventDefault()
-    console.log('1')
+ 
     li=document.createElement('li')
     li.className='list-group-item'
+
+    
   
-   z=document.createTextNode(usertext)
-   li.appendChild(z)
+  
   
   
 
@@ -24,8 +29,21 @@ function handleSubmit(e)
    btn.className='btn btn-danger btn-sm float-right delete'
    btn.appendChild(document.createTextNode('X'))
 
+
+   z=document.createTextNode(usertext)
+   li.appendChild(z)
+
  
   li.appendChild(btn)
+  itemsList.appendChild(li)
+
+
+
+//adding discription 
+  input2=document.getElementById('item2').value
+  mytextNode=document.createTextNode(" "+input2)
+  li.appendChild(mytextNode)
+
   itemsList.appendChild(li)
 
    
@@ -37,7 +55,7 @@ function removeItem(e)
       if(confirm('are you sure'))
       {
         var li=e.target.parentElement
-      itemsList.removeChild(li)
+      itemsList.removeChild( li)
       }
     }
 }
@@ -47,16 +65,25 @@ firstElement=document.getElementsByClassName('list-group-item')
 var filter=document.getElementById('filter')
 filter.addEventListener('keyup',filterItems)
 
+
 function filterItems(e)
 {
 inputText=e.target.value.toLowerCase()
+
+
+
+
 items=document.getElementsByTagName('li')
 xx=convertedToarray=Array.from(items)
 xx.forEach((item)=>{
 
   mycontent =item.firstChild.textContent
-  console.log(mycontent);
-  if(mycontent.toLowerCase().includes(inputText))
+  // console.log(mycontent);
+  secondContent=item.childNodes[1].textContent
+  console.log(secondContent);
+  // mycontent=item.secondChild.textContent
+
+  if(mycontent.toLowerCase().indexOf(inputText)!= -1 || secondContent.toLowerCase().indexOf(inputText) != -1)
   {
     item.style.display='block'
   }else{
@@ -65,3 +92,7 @@ xx.forEach((item)=>{
 })
 // console.log(xx ); 
 }
+
+
+// console.log(input2);
+
