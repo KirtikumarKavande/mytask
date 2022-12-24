44eb3d5bcad48192a8f11ddca8e514b4520ce6b1
KirtikumarKavande <kirtikumar0005@gmail.com>
2022-12-24 23:40:02 +0530

    multiple user form with localstorage

--- form/script3.js
+++ form/script3.js
@@ -1,6 +1,6 @@
 function getValue(event)
 {
-  console.log(event)
+//   console.log(event)
     event.preventDefault()
 
     // localStorage.setItem('name',event.target.username.value)
@@ -8,25 +8,42 @@ function getValue(event)
     // localStorage.setItem('phone',event.target.phone.value)
     // localStorage.setItem('time',event.target.time.value)
 
+
     myobj={
         name:event.target.username.value,
         email:event.target.email.value,
         phone:event.target.phone.value,
         time:event.target.time.value
     }
-    localStorage.setItem('myobj',JSON.stringify(myobj))
     
-console.log(event.target.username.value)
+    localStorage.setItem(myobj.email,JSON.stringify(myobj))
+    
+// console.log(event.target.username.value)
 
-console.log(event.target.email.value)
+// console.log(event.target.email.value)
 
-console.log(event.target.phone.value)
-console.log(event.target.date.value)
-console.log(event.target.time.value)
+// console.log(event.target.phone.value)
+// console.log(event.target.date.value)
+// console.log(event.target.time.value)
+
+// userList=document.getElementById('userList')
+// console.log(userList);
+
+// eachUser=localStorage.getItem(myobj.email)
+// detailDisplay=document.createTextNode(eachUser.name)
+// console.log(detailDisplay);
+
+// userList.appendChild(detailDisplay)
+showNewUserOnScreen(myobj)
 
 
 
 
 
-
+}
+function showNewUserOnScreen(user)
+{
+    parentNode=document.getElementById( 'listOfUsers' )
+    const childHTML=`<li>${user.name} -${user.email} </li>`
+    parentNode.innerHTML=parentNode.innerHTML +childHTML
 }
