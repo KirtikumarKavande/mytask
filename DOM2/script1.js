7d99ec3be592f15d8eb7a834cf2ded577c828d2b
KirtikumarKavande <kirtikumar0005@gmail.com>
2022-12-24 15:19:29 +0530

    added search functionality & another input

--- DOM2/script1.js
+++ DOM2/script1.js
@@ -1,98 +1,81 @@
-myform=document.querySelector('#addForm')
-
-myform.addEventListener('submit',handleSubmit)
-itemsList=document.getElementById('items')
-//delete
-itemsList.addEventListener('click',removeItem)
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  //taking value from second input
+  var input2=document.getElementById('item2').value
 
 
+  //adding another text node
+  li.appendChild(document.createTextNode(' '+ input2))
 
+  // Create del button element
+  var deleteBtn = document.createElement('button');
 
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-function handleSubmit(e)
-{
-    usertext=document.getElementById('item').value
-    e.preventDefault()
- 
-    li=document.createElement('li')
-    li.className='list-group-item'
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
 
-    
-  
-  
-  
-  
+  // Append button to li
+  li.appendChild(deleteBtn);
 
-   btn=document.createElement('button')
-   btn.className='btn btn-danger btn-sm float-right delete'
-   btn.appendChild(document.createTextNode('X'))
-
-
-   z=document.createTextNode(usertext)
-   li.appendChild(z)
-
- 
-  li.appendChild(btn)
-  itemsList.appendChild(li)
-
-
-
-//adding discription 
-  input2=document.getElementById('item2').value
-  mytextNode=document.createTextNode(" "+input2)
-  li.appendChild(mytextNode)
-
-  itemsList.appendChild(li)
-
-   
+  // Append li to list
+  itemList.appendChild(li);
 }
-function removeItem(e)
-{
-    if(e.target.classList.contains('delete'))
-    {
-      if(confirm('are you sure'))
-      {
-        var li=e.target.parentElement
-      itemsList.removeChild( li)
-      }
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
     }
-}
-
-//filter
-firstElement=document.getElementsByClassName('list-group-item')
-var filter=document.getElementById('filter')
-filter.addEventListener('keyup',filterItems)
-
-
-function filterItems(e)
-{
-inputText=e.target.value.toLowerCase()
-
-
-
-
-items=document.getElementsByTagName('li')
-xx=convertedToarray=Array.from(items)
-xx.forEach((item)=>{
-
-  mycontent =item.firstChild.textContent
-  // console.log(mycontent);
-  secondContent=item.childNodes[1].textContent
-  console.log(secondContent);
-  // mycontent=item.secondChild.textContent
-
-  if(mycontent.toLowerCase().indexOf(inputText)!= -1 || secondContent.toLowerCase().indexOf(inputText) != -1)
-  {
-    item.style.display='block'
-  }else{
-    item.style.display='none'
   }
-})
-// console.log(xx ); 
 }
 
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
 
-// console.log(input2);
 
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+//adding second field in search bar
+var itemName2=item.childNodes[1].textContent
+
+
+    if(itemName.toLowerCase().indexOf(text) != -1 || itemName2.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
