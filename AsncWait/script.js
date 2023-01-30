ad3275ebac3b5b1cc66719abd4c99323086060ab
KirtikumarKavande <121145138+KirtikumarKavande@users.noreply.github.com>
2023-01-30 11:25:52 +0530

    task 4 commit

--- AsncWait/script.js
+++ AsncWait/script.js
@@ -87,26 +87,118 @@
 // console.log("person4:shows ticket");
 // console.log("person5:shows ticket");
 
-async function preMovie() {
-  task1 = new Promise((resolve, reject) => {
-    resolve("cold drinks");
-  });
-  task2 = new Promise((resolve, reject) => {
-    resolve("chinease");
-  });
-  task3 = new Promise((resolve, reject) => {
-    resolve("popcorn");
-  });
+// async function preMovie() {
+//   task1 = new Promise((resolve, reject) => {
+//     resolve("cold drinks");
+//   });
+//   task2 = new Promise((resolve, reject) => {
+//     resolve("chinease");
+//   });
+//   task3 = new Promise((resolve, reject) => {
+//     resolve("popcorn");
+//   });
 
-  const mytask1 = await task1;
-  const mytask2 = await task2;
-  const mytask3 = await task3;
-  //    [coldDrinks,chinease,popcorn]=await Promise.all([mytask1,mytask2,mytask3])
-  //     console.log(coldDrinks);
+//   const mytask1 = await task1;
+//   const mytask2 = await task2;
+//   const mytask3 = await task3;
+//   //    [coldDrinks,chinease,popcorn]=await Promise.all([mytask1,mytask2,mytask3])
+//   //     console.log(coldDrinks);
 
-  return Promise.all([mytask1, mytask2, mytask3]);
+//   return Promise.all([mytask1, mytask2, mytask3]);
+// }
+
+// preMovie().then((value) => {
+//   console.log(value);
+// });
+
+posts = [
+  {
+    title: "first book",
+    price: 100,
+    time: new Date(),
+  },
+  {
+    title: "second book",
+    price: 200,
+    time: new Date(),
+  },
+];
+
+function getposts() {
+  setTimeout(() => {
+    let output = "";
+
+    posts.forEach((post) => {
+      output += `<li>${post.title} </li>`;
+    });
+
+    document.body.innerHTML = output;
+  }, 1000);
 }
 
-preMovie().then((value) => {
-  console.log(value);
+async function createPost(post) {
+  const create = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+      const err = false;
+      if (!err) {
+        resolve();
+      } else {
+        reject("error something went wrong");
+      }
+    }, 2000);
+  });
+
+  myCreatePost=await create;
+  return myCreatePost;
+
+  
+}
+
+
+
+async function deletePost() {
+    const deletep=new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            if (posts.length > 0) {
+              posts.pop();
+              resolve();
+            } else {
+              reject("array is empty now");
+            }
+          }, 1000); 
+    })
+    myDeletePost=await deletep;
+    return myDeletePost;
+
+}
+
+createPost({ title: "third book", price: 300 }).then(function () {
+  getposts();
+  deletePost().then(function () {
+    getposts();
+    deletePost().then(function () {
+      getposts();
+      deletePost().then(function () {
+        getposts();
+        deletePost().catch(function (err) {
+          console.log("error occured", err);
+        });
+      });
+    });
+  });
 });
+
+setTimeout(() => {
+  createPost({ title: "forth book", price: 400 }).then(function () {
+    getposts();
+
+    deletePost().then(function () {
+      getposts();
+    });
+  });
+}, 7000);
+
+
+
